e7246466189b4eec8c86107fb4eeba0e11bcb7a4
jangbigom91 <jangbigom91@gmail.com>
2020-06-24 17:02:08 +0900

    2020/06/24 등록

--- Ch08/8-1.py
+++ Ch08/8-1.py
@@ -7,9 +7,9 @@ import pymysql as db
 
 # 데이터베이스 접속
 conn = db.connect(host='192.168.44.46',
-                   user='chhak',
+                   user='cjh',
                    password='1234',
-                   db='chhak',
+                   db='cjh',
                    charset='utf8')
 
 # 쿼리문 실행객체 생성
--- Ch08/8-2.py
+++ Ch08/8-2.py
@@ -1,20 +1,20 @@
 import pymysql as db
 
 conn = db.connect(host='192.168.44.46',
-                  user='chhak',
+                  user='cjh',
                   password='1234',
-                  db='chhak',
+                  db='cjh',
                   charset='utf8')
 
 cur = conn.cursor()
 cur.execute("SELECT * FROM `USER1`")
 
 for row in cur.fetchall():
-    print('-----------------')
+    print('--------------------')
     print('아이디 :', row[0])
     print('이름 :', row[1])
     print('휴대폰 :', row[2])
     print('나이 :', row[3])
-    print('-----------------')
+    print('--------------------')
 
 conn.close()
